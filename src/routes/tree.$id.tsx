9bf226d8e88d5abfe2cbf5ad36d0c75f98ee9bf8
corelladionne-crypto <corelladionne@gmail.com>
2026-08-23 10:47:29 -0700

    Use localStorage-only tree route

--- src/routes/tree.$id.tsx
+++ src/routes/tree.$id.tsx
@@ -1,17 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { getTree } from "@/lib/tree.server";
-import { RootedExperience } from "./index";
+import { LocalTreeExperience } from "@/components/rooted/local-tree-experience";
 
 export const Route = createFileRoute("/tree/$id")({
-  staleTime: 0,
-  loader: async ({ params }) => getTree({ data: { id: params.id } }),
   component: GuestTreeRoute,
 });
 
 function GuestTreeRoute() {
   const { id } = Route.useParams();
-  const tree = Route.useLoaderData();
-
-  return <RootedExperience guestId={id} existingTree={tree} />;
+  return <LocalTreeExperience guestId={id} />;
 }
